878ead309bfa3571aabd9fdee2c57d56b6391b6d
Diego Garcia Perez <diego@gsdpi-3090.edv.uniovi.es>
2023-11-27 19:10:04 +0100

    new metrics nep and eac

--- metrics.py
+++ metrics.py
@@ -5,3 +5,11 @@ def mae(y_true,y_pred):
 
 def rmse(y_true,y_pred):
     return np.square(np.mean((y_true-y_pred)**2))
+
+# Normalize error in assigned power
+
+def nep(y_true,y_pred):
+    return np.sum(np.abs(y_true-y_pred))/np.sum(y_true)
+
+def eac(y_true,y_pred):
+    return 1- np.sum(np.abs(y_true-y_pred))/(np.sum(y_true)*2)
